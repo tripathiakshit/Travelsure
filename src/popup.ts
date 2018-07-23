7e4fdab56f931c44ca8f98cec92ee31c33c66f37
Akshit Tripathi <tripathisoft@gmail.com>
2018-07-22 23:29:54 -0400

    Updated popup to include sign up

--- src/popup.ts
+++ src/popup.ts
@@ -3,30 +3,35 @@ import * as $ from 'jquery';
 
 let count = 0;
 
-$(function() {
+$(function () {
   const queryInfo = {
     active: true,
     currentWindow: true
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
+  chrome.tabs.query(queryInfo, function (tabs) {
     $('#url').text(tabs[0].url);
     $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
   });
 
-  chrome.browserAction.setBadgeText({text: count.toString()});
-  $('#countUp').click(()=>{
-    chrome.browserAction.setBadgeText({text: (++count).toString()});
+  chrome.browserAction.setBadgeText({ text: count.toString() });
+  $('#countUp').click(() => {
+    chrome.browserAction.setBadgeText({ text: (++count).toString() });
   });
 
-  $('#changeBackground').click(()=>{
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  $('#changeBackground').click(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         color: '#555555'
       },
-      function(msg) {
-        console.log("result message:", msg);
-      });
+        function (msg) {
+          console.log("result message:", msg);
+        });
     });
   });
 });
+
+function signUp(): void {
+  bg.setUsername($('#username'));
+  bg.setEmail($('#email'));
+}
