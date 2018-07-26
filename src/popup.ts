1b92e0fa9f7e8549e06ed3ca22d1ff9c5edf6b4c
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2018-07-25 20:14:40 -0400

    Fixed popup navigation
    Signed-off-by: Saurabh Joshi <saurabhsjoshi94@gmail.com>

--- src/popup.ts
+++ src/popup.ts
@@ -3,67 +3,31 @@ import * as $ from 'jquery';
 //@ts-ignore
 var UsaStates = require('usa-states').UsaStates;
 
-let count = 0;
-let states = new UsaStates();
-
-$(function () {
-  const queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function (tabs) {
-    $('#url').text(tabs[0].url);
-    $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-  });
-
-  chrome.browserAction.setBadgeText({ text: count.toString() });
-  $('#countUp').click(() => {
-    chrome.browserAction.setBadgeText({ text: (++count).toString() });
-  });
-
-  $('#changeBackground').click(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        color: '#555555'
-      },
-        function (msg) {
-          console.log("result message:", msg);
-        });
+chrome.storage.local.get(['userDob', 'userState'], function(result) {
+  if(result.userDob && result.userState) {
+    window.location.href="insurance_policy.html";
+  } else {
+    let states = new UsaStates();
+    let select = document.getElementById("usStateSelect");
+    let abbrs = states.arrayOf('a');
+    let names = states.arrayOf('s');
+    
+    for (let i = 0; i < abbrs.length; i++) {
+      let opt = document.createElement("option");
+      opt.setAttribute('value', abbrs[i]);
+      opt.innerText = names[i];
+      select.appendChild(opt);
+    }
+    $("#userDetailForm").submit(function(event) {
+      event.preventDefault();
+      let userDob = $('#userDob').val();
+      let userState = $("#usStateSelect").val();
+      chrome.storage.local.set ({
+        'userDob': userDob,
+        'userState': userState
+      }, function() {
+        console.log("Saved!");
+      });
     });
-  });
-
-  let select = document.getElementById("usStateSelect");
-  
-  let abbrs = states.arrayOf('a');
-  let names = states.arrayOf('s');
-  for (let i = 0; i < abbrs.length; i++) {
-    let opt = document.createElement("option");
-    opt.setAttribute('value', abbrs[i]);
-    opt.innerText = names[i];
-    select.appendChild(opt);
   }
 });
-
-chrome.storage.local.get(['userDob', 'userState'], function(result) {
-  let select = document.getElementById("usStateSelect");
-  let aIndex = states.arrayOf('a').indexOf(result.userState);
-  let stateName = states.arrayOf('s')[aIndex];
-  select.setAttribute("selected", stateName); 
-});
-
-$("#userDetailForm").submit(function(event) {
-  event.preventDefault();
-  let userDob = $('#userDob').val();
-  let userState = $("#usStateSelect").val();
-  chrome.storage.local.set ({
-    'userDob': userDob,
-    'userState': userState
-  }, function() {
-    console.log("Saved!");
-  });
-});
-
-function submitUserDetail() {
-  let form = $('#userDetail');
-}
