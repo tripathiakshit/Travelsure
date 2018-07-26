720b58739b3b54a68040d2a2e656d885db8dc46a
Akshit Tripathi <tripathisoft@gmail.com>
2018-07-25 22:57:27 -0400

    Added additional pages to insurance policy flow, and created page linking

--- src/insurance_policy.ts
+++ src/insurance_policy.ts
@@ -1,2 +1,39 @@
 import * as $ from 'jquery';
 
+let selectedPlan = null;
+let state = "USA";
+
+$('#travel-country').val(state);
+
+$('#atp-button').click((event) => {
+    selectedPlan = "atp";
+    console.log(selectedPlan);
+    $('#page1').hide(
+        "fast",
+        () => {
+            $('#page2').show("fast", "slide");
+            // Load Air Travel Protection data
+        });
+});
+
+$('#premier-button').click((event) => {
+    selectedPlan = "premier";
+    console.log(selectedPlan);
+    $('#page1').hide(
+        "fast",
+        () => {
+            $('#page2').show("fast", "slide");
+            // Load Premier Plan data
+        });
+});
+
+$('#classic-button').click((event) => {
+    selectedPlan = "classic";
+    console.log(selectedPlan);
+    $('#page1').hide(
+        "fast",
+        () => {
+            $('#page2').show("fast", "slide");
+            // Load classic plan trailer
+        });
+});
