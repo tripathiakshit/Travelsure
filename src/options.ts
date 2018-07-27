c8a829d5b1d71b9be4ad2020a2ad53a402f55652
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2018-07-26 23:32:18 -0400

    Fixed options page
    Signed-off-by: Saurabh Joshi <saurabhsjoshi94@gmail.com>

--- src/options.ts
+++ src/options.ts
@@ -1,34 +1,8 @@
-import * as moment from 'moment';
 import * as $ from 'jquery';
 
 // Saves options to chrome.storage.sync.
-function save_options() {
-  let color = $('#color').val();
-  let likesColor = $('#like').prop('checked');
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function () {
-    // Update status to let user know options were saved.
-    var status = $('#status');
-    status.text('Options saved.');
-    setTimeout(function () {
-      status.text('');
-    }, 750);
-  });
+function clearSettings() {
+  chrome.storage.local.clear();
+  window.close();
 }
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get(['favoriteColor', 'likesColor', 'userDob', 'userState'], function (items) {
-    $('#color').val(items.favoriteColor);
-    $('#like').prop('checked', items.likesColor);
-    $('#currentUserDob').val(items.userDob);
-    $('#currentUserState').val(items.userState);
-  });
-}
-
-$('#save').click(save_options);
-$(restore_options); // document.addEventListener('DOMContentLoaded', restore_options);
+$('#save').click(clearSettings);
